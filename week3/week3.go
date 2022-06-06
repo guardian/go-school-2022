3fa1933acd7bc2dea7d88ff7c57c07b38dda22e3
Nicolas Long <nicolas.long@theguardian.com>
2022-06-06 10:23:37 +0100

    Add week3 hints

--- week3/week3.go
+++ week3/week3.go
@@ -1,18 +1,30 @@
 package week3
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
-// make the tests pass
+// Make the tests pass by running `go test ./week3` until it's happy.
+//
+// Use the cheatsheet to help:
+// https://docs.google.com/document/d/1kHskYWl64DsMmI-dRKLKr-SN5B865up4tL7AoM2biHg/edit?usp=sharing.
 
 // FizzBuzz prints consecutive numbers from 0 until upto (exclusive). Numbers
 // divisible by 3 are replaced by 'Fizz' and numbers divisible by 5 replaced by
 // 'Buzz'. For numbers divisible by both 3 and 5 'Fizz Buzz' is written.
+//
+// Hints: you can use strconv.Itoa to convert an int to a string. fmt.Fprintf is
+// also a good option here. Consider using a switch to check the cases.
 func FizzBuzz(w io.Writer, upto int) {
-	w.Write([]byte("TODO"))
+	fmt.Fprint(w, "TODO")
 }
 
 // Find returns the first element in items that satisfies a predicate function
 // 'p'.
+//
+// Hints: for/range is your friend here. You can use `var t T` to initialise the
+// unknown type to its 'zero value'.
 func Find[T any](items []T, p func(T) bool) T {
 	return items[0] // FIXME
 }
@@ -22,12 +34,19 @@ type Item struct {
 	URL   string
 }
 
-// GetNews executes an HTTP GET request to :host/news, parses the JSON into a list of Items, and return it.
+// GetNews executes an HTTP GET request to :host/news, parses the JSON into a
+// list of Items, and return it.
 //
-// - if the HTTP request fails, return an error containing the string 'http request failed'
-// - if the JSON parsing fails, return an error containing the string 'json parsing failed'.
+// - if the HTTP request fails, return an error containing the string 'http
+// request failed' - if the JSON parsing fails, return an error containing the
+// string 'json parsing failed'.
 //
 // (In real life, we would want to use better/custom errors here).
+//
+// Hints: use the `http` package for your GET request. Use the `encoding/json`
+// package to 'Unmarshal' the response into a slice of the `Item` type defined
+// above. Use the io.ReadAll helper to read the response body. &T (where T is
+// some type) can be used to get a pointer for it.
 func GetNews(host string) ([]Item, error) {
 	return []Item{}, nil // TODO
 }
