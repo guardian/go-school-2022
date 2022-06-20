176474346da28fa5555aebe9c0cd7fbd3dda0895
Nicolas Long <nicolas.long@theguardian.com>
2022-06-20 11:58:15 +0100

    Reset

--- week5/week5.go
+++ week5/week5.go
@@ -8,47 +8,15 @@ import (
 
 // FanIn should merge channels into a single read-only channel.
 func FanIn[T any](chs ...chan T) <-chan T {
-	out := make(chan T)
-
-	for _, ch := range chs {
-		go func(ch chan T) {
-			for item := range ch {
-				out <- item
-			}
-		}(ch)
-	}
-
-	return out
+	ch := make(chan T)
+	return ch
 }
 
 // Throttle should return a function that, however often called, executes no
 // more than once per wait time.
 // Hint: lock := make(chan bool, 1) can be used as a semaphore here.
 func Throttle[A, B any](fn func(), tick chan B) func() {
-	lock := make(chan bool, 1)
-	lock <- true
-
-	// first time it should return straight away
-	// after that, it should
-	go func() {
-		for range tick {
-			select {
-			case lock <- true:
-				// free up lock
-			default:
-				// do nothing
-			}
-		}
-	}()
-
-	return func() {
-		select {
-		case <-lock:
-			fn()
-		default:
-			// do nothing
-		}
-	}
+	return func() {}
 }
 
 // Cancellable wraps a function to provide cancellation (via a context). Note,
@@ -59,24 +27,8 @@ func Throttle[A, B any](fn func(), tick chan B) func() {
 // Hint: the ctx.Done() chan can be read to check for cancellation.
 // Hint: make sure you *close* the channel upon cancel.
 func Cancellable[A any](ctx context.Context, fn func() chan A) func() chan A {
-	ch := make(chan A)
-
 	return func() chan A {
-		fnOut := fn()
-
-		go func() {
-			select {
-			case <-ctx.Done():
-				close(ch)
-			case item, ok := <-fnOut:
-				if !ok {
-					close(ch)
-					return
-				}
-				ch <- item
-			}
-		}()
-
+		ch := make(chan A)
 		return ch
 	}
 }
